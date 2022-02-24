8f8180948924d9c25f4705906dac08af96491c76
REYNOLDS <4800556183@davistech.edu>
2022-02-24 12:54:36 -0700

    Finished Project

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject {
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text here";
+
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion() => question;
+    public int GetCorrectAnswerIndex() => correctAnswerIndex;
+    public string GetAnswer(int index) => answers[index];
+}
